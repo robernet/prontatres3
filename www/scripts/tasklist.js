df937caafff3f3df1649628de7e2e78603e86bad
RoberNET <robernet@gmail.com>
2018-04-03 17:53:30 -0500

    Add task list

---
+++ www/scripts/tasklist.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var AddNewTask = $('#add-new-task');
+    var NewTaskInput = $('#new-task-input');
+    var $tasksSection = $('#tasks-section');
+
+    // <a href="#" class="tasklist-item tasklist-red tasklist-completed">
+    //      <i class="ion-checkmark"></i>
+    //      <h5>Prepare a Design</h5>
+    // </a>                    
+
+});
